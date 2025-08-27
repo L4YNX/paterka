33a3a64ba9b538351c5723e4ca1f418ce4f3adce
L4YNX <83310060+L4YNX@users.noreply.github.com>
2025-08-27 10:20:44 +0200

    dziala

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import MainLayOut from './layout/MainLayOut';
 import HomePage from './pages/HomePage';
 import Oferta from './pages/OfertaPage';
 import KontaktPage from './pages/KontaktPage';
 import PageNotFound from './components/PageNotFound';
 
-const isGitHubPages = window.location.hostname.includes('github.io');
-
-const App = () => {
-  const Router = isGitHubPages ? HashRouter : BrowserRouter;
-
-  return (
-    <Router>
-      <Routes>
-        <Route path="paterka/" element={<MainLayOut />}>
-          <Route index element={<HomePage />} />
-          <Route path="oferta" element={<Oferta />} />
-          <Route path="kontakt" element={<KontaktPage />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
-};
-
+const App = () => (
+  <HashRouter>
+    <Routes>
+      <Route path="/paterka" element={<MainLayOut />}>
+        <Route index element={<HomePage />} />
+        <Route path="oferta" element={<Oferta />} />
+        <Route path="kontakt" element={<KontaktPage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    </Routes>
+  </HashRouter>
+);
 
 export default App;
