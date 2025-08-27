be8b3808c5f9975d09270cd1a0a69b079f04cde8
L4YNX <83310060+L4YNX@users.noreply.github.com>
2025-08-27 11:26:39 +0200

    moze dziala

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,20 @@ import HomePage from './pages/HomePage';
 import Oferta from './pages/OfertaPage';
 import KontaktPage from './pages/KontaktPage';
 import PageNotFound from './components/PageNotFound';
-import OfertaPage from './pages/OfertaPage';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path='/paterka' element={<MainLayOut/>}>
-    <Route index element={<Home/>}/>
-    <Route path='/oferta' Component={<OfertaPage/>}/>
-    <Route path='/kontakt' Component={<KontaktPage/>}/>
-    <Route path='*' element={<PageNotFound/>}/>
-  </Route>
-  )
-);
 
 const App = () => {
-  return <RouterProvider router={router}/>
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<MainLayOut />}>
+          <Route index element={<HomePage />} />
+          <Route path='/oferta' element={<Oferta />} />
+          <Route path='/kontakt' element={<KontaktPage />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
 };
 
 export default App;
